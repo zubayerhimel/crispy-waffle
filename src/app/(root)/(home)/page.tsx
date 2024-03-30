e4f693f27785b729bcf17afed7c93f3599cc463f
zubayerhimel <zhimel007@gmail.com>
2024-03-30 15:05:41 +0600

    chore: add date time dynamically

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,7 +1,19 @@
+import dayjs from 'dayjs';
+
 const Home = () => {
+  const dateTime = new Date();
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>Home</h1>
+      <div className='h-[300px] w-full rounded-xl bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between p-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming meeting at: 12:00 PM</h2>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-bold lg:text-6xl'>{dayjs(dateTime).format('hh:mm A')}</h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{dayjs(dateTime).format('dddd, MMMM DD, YYYY')}</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,20 @@ const config = {
         blue: {
           1: '#0E78F9',
         },
+        sky: {
+          1: '#C9DDFF',
+          2: '#ECF0FF',
+          3: '#F5FCFF',
+        },
+        orange: {
+          1: '#FF742E',
+        },
+        purple: {
+          1: '#830EF9',
+        },
+        yellow: {
+          1: '#F9A90E',
+        },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
@@ -54,6 +68,9 @@ const config = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')",
       },
       borderRadius: {
         lg: 'var(--radius)',
