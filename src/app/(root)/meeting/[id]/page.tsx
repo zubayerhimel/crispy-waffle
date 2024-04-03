06f71aa374a1d57390959dabfa12863e8d34a894
zubayerhimel <zhimel007@gmail.com>
2024-04-03 13:00:06 +0600

    chore: enable mic and camera for streaming

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -6,15 +6,22 @@ import { useState } from 'react';
 
 import MeetingRoom from '@/components/application/meeting-room';
 import MeetingSetup from '@/components/application/meeting-setup';
+import { useGetCallById } from '@/hooks/use-get-call-by-id';
+import Loader from '@/components/application/loader';
 
-const meetings = ({ params }: { params: { id: string } }) => {
+const meetings = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
 
   const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) {
+    return <Loader />;
+  }
 
   return (
     <main className='h-screen w-full'>
-      <StreamCall>
+      <StreamCall call={call}>
         <StreamTheme>{!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}</StreamTheme>
       </StreamCall>
     </main>
--- src/components/application/meeting-setup.tsx
+++ src/components/application/meeting-setup.tsx
@@ -1,5 +1,36 @@
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk';
+import { useEffect, useState } from 'react';
+
 const MeetingSetup = () => {
-  return <h1>Meeting setup</h1>;
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error('useCall must be used within StreamCall component');
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+
+  return (
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+      <h1 className='text-2xl font-bold'>Setup</h1>
+      <VideoPreview />
+      <div className='flex h-16 items-center justify-center gap-2 font-medium'>
+        <label htmlFor='mic-and-camera' className='flex items-center justify-center gap-2 font-medium'>
+          <input type='checkbox' name='' id='mic-and-camera' checked={isMicCamToggledOn} onChange={(e) => setIsMicCamToggledOn(e.target.checked)} />
+          Join with mic and camera off
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default MeetingSetup;
