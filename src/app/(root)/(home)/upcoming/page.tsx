6231bd6569b6c16f75b9e61960028557ee95c3e0
zubayerhimel <zhimel007@gmail.com>
2024-04-04 16:25:56 +0600

    chore: add meeting card to the call list component

--- src/app/(root)/(home)/upcoming/page.tsx
+++ src/app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import CallList from '@/components/application/call-list';
 
 const Upcoming = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>Upcoming</h1>
+      <CallList type='upcoming' />
     </section>
   );
 };
