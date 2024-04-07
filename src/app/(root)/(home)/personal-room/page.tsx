d8450297927e3d0d8e2054a03aadf812bfc27002
Zubayer <zhimel007@gmail.com>
2024-04-07 16:31:20 +0600

    feat: redirect to home page after leaving meeting

--- src/app/(root)/(home)/personal-room/page.tsx
+++ src/app/(root)/(home)/personal-room/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { useGetCallById } from "@/hooks/use-get-call-by-id";
+import { useRouter } from "next/navigation";
+
+const Table = ({ title, description }: { title: string; description: string }) => {
+  return (
+    <div className='flex flex-col items-start gap-2 xl:flex-row'>
+      <h1 className='text-base font-medium text-sky-1 lg:text-xl xl:min-w-32'>{title}:</h1>
+      <h1 className='truncate text-sm font-semibold max-sm:max-w-[320px] lg:text-xl'>{description}</h1>
+    </div>
+  );
+};
 
 const PersonalRoom = () => {
+  const { user } = useUser();
+  const meetingId = user?.id;
+  const { toast } = useToast();
+  const { call } = useGetCallById(meetingId!);
+  const client = useStreamVideoClient();
+  const router = useRouter();
+
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+
+  const startMeeting = async () => {
+    if (!client || !user) return;
+
+    const newCall = client.call("default", meetingId!);
+
+    if (!call) {
+      await newCall?.getOrCreate({
+        data: {
+          starts_at: new Date().toISOString(),
+        },
+      });
+    }
+
+    router.push(`/meeting/${meetingId}?personal=true`);
+  };
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>Personal Room</h1>
+      <div className='flex flex-col w-full'>
+        <Table title='Topic' description={`${user?.username}s meeting room`} />
+        <Table title='Meeting ID' description={meetingId!} />
+        <Table title='Invite Link' description={meetingLink} />
+      </div>
+      <div className='flex gap-5'>
+        <Button className='bg-blue-1' onClick={startMeeting}>
+          Start Meeting
+        </Button>
+        <Button
+          className='bg-dark-3'
+          onClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast({
+              title: "Link Copied",
+            });
+          }}>
+          Copy invitation
+        </Button>
+      </div>
     </section>
   );
 };
--- src/components/application/meeting-room.tsx
+++ src/components/application/meeting-room.tsx
@@ -1,20 +1,21 @@
-import { cn } from '@/lib/utils';
-import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
-import { LayoutList, Users } from 'lucide-react';
-import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { cn } from "@/lib/utils";
+import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { LayoutList, Users } from "lucide-react";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
-import EndCallButton from './end-call-button';
-import Loader from './loader';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import EndCallButton from "./end-call-button";
+import Loader from "./loader";
 
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
-  const isPersonalRoom = !!searchParams.get('personal');
+  const isPersonalRoom = !!searchParams.get("personal");
+  const router = useRouter();
 
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState<boolean>(false);
 
   const { useCallCallingState } = useCallStateHooks();
@@ -24,11 +25,11 @@ const MeetingRoom = () => {
 
   const CallLayout = () => {
     switch (layout) {
-      case 'grid':
+      case "grid":
         return <PaginatedGridLayout />;
-      case 'speaker-right':
+      case "speaker-right":
         return <SpeakerLayout participantsBarPosition='left' />;
-      case 'speaker-left':
+      case "speaker-left":
         return <SpeakerLayout participantsBarPosition='right' />;
       default:
         return <SpeakerLayout participantsBarPosition='right' />;
@@ -41,12 +42,12 @@ const MeetingRoom = () => {
         <div className='flex size-full max-w-[1000px] items-center'>
           <CallLayout />
         </div>
-        <div className={cn('h-[calc(100vh-86px)] hidden ml-2', { 'show-block': showParticipants })}>
+        <div className={cn("h-[calc(100vh-86px)] hidden ml-2", { "show-block": showParticipants })}>
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
       <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap mb-2'>
-        <CallControls />
+        <CallControls onLeave={() => router.push("/")} />
 
         <DropdownMenu>
           <div className='flex items-center'>
@@ -55,7 +56,7 @@ const MeetingRoom = () => {
             </DropdownMenuTrigger>
           </div>
           <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
-            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
               <div key={index}>
                 <DropdownMenuItem onClick={() => setLayout(item.toLowerCase() as CallLayoutType)} className='cursor-pointer'>
                   {item}
